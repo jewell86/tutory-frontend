ef084bedb99d5399150c3f3fa34b12a35608f002
Nate <ndsunc@gmail.com>
2018-08-08 13:40:53 -0700

    Added link in loggedin nav to redirect to new tutorial page

--- scripts/templates/nav.js
+++ scripts/templates/nav.js
@@ -14,7 +14,8 @@ function loggedInNavTemplate() {
         </div>
         </form>
         </li>
-        <li><a href="#" class="nav-my-tutorials">My Tutorials</a></li>
+        <li><a href="#" class="nav-create-tutorial">New Tutorial</a></li>
+        <li><a href="#" class="nav-my-tutorials">Watchlist</a></li>
         <li><a href="#" class="nav-my-profile">My Profile</a></li>
       </ul>
     </div>
@@ -56,7 +57,7 @@ function navTemplate() {
                                     <div class="form-group">
                                          <button class="dropdown-toggle btn btn-primary btn-block login-submit" data-toggle="dropdown">Login</button>
                                     </div>
-                
+
                              </form>
                         </div>
 

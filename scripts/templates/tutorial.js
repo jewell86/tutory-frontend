894590a8c19ab0c4a6173c56ac96d0755e920c0d
Branden <b.lowe333@gmail.com>
2018-08-09 11:22:57 -0700

    generating a star rating for a tutorial

--- scripts/templates/tutorial.js
+++ scripts/templates/tutorial.js
@@ -1,7 +1,12 @@
-function tutorialPageTemplate(id, userId, title, description, instructorBio, instructorImage ) {
+function tutorialPageTemplate(id, userId, title, description, instructorBio, instructorImage) {
     return `
     <div data-id="${id}" data-userid="${userId}"class="container">
-        <h1>${title}</h1>
+    <h1>${title}</h1>
+        <div class="star-rating-div">
+            <div class="stars-outer">
+                <div class="stars-inner"></div>
+            </div>
+        </div>
         <h5>${description}</h5>
         <button class="add-tutorial">Add</button>
         <div class="videos"></div>
@@ -18,14 +23,14 @@ function tutorialPageTemplate(id, userId, title, description, instructorBio, ins
     `
 }
 
-function videosTemplate (video) {
- return `
+function videosTemplate(video) {
+    return `
  <iframe width="420" height="315" src="${video}" frameborder="0" allowfullscreen></iframe>
 `
 }
 
-function commentsTemplate (comment) {
-   return ` 
+function commentsTemplate(comment) {
+    return ` 
                 <h6>${comment}</h6>
            `
 }

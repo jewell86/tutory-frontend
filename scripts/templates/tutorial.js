710d32a59fb187beca7d1b36ca30617694f3dce6
Nate <ndsunc@gmail.com>
2018-08-09 20:35:02 -0700

    The about the author part of the tutorials page is done

--- scripts/templates/tutorial.js
+++ scripts/templates/tutorial.js
@@ -1,33 +1,38 @@
 const moment = require('moment')
 
 function tutorialPageTemplate(id, userId, title, description, instructorBio, instructorImage ) {
-    return `
-    <div  data-userid="${userId}"class="container">
-    <h1>${title}</h1>
-        <div class="star-rating-div">
-            <div class="stars-outer">
-                <div class="stars-inner"></div>
-            </div>
+  return `
+  <div data-userid="${userId}" class="container">
+    <div class="tutorial-container">
+      <h1 class="tutorial-header">${title}</h1>
+      <div class="star-rating-div">
+        <div class="stars-outer">
+          <div class="stars-inner"></div>
         </div>
-        <h5>${description}</h5>
-        <a class="btn-floating waves-effect waves-light red add-button"><i class="material-icons" data-id="${id}">add</i></a>
-        <div class="videos"></div>
-        <div class="instructor-info">
-            <h2>Instructor</h2>
-            <img height="300" width="200" src="${instructorImage}">
-            <h3>About me</h3>
-            <h6>${instructorBio}</h6>
-        </div>
-        <h3>COMMENTS</h3>
-
-        <div class="comments-div">
-          <div class="comments"></div>
-          <div class="add-comment-btn-div">
-            <button class="add-comment">Add Comment</button>
-          </div>
-        </div>
+      </div>
+      <h5>${description}</h5>
+      <a class="btn-floating waves-effect waves-light red add-button"><i class="material-icons" data-id="${id}">add</i></a>
+      <div class="videos"></div>
     </div>
-    `
+
+    <div class="instructor-info">
+      <img class="usr-img" src="${instructorImage}">
+      <div class="instructor-bio">
+        <h4>Tutorial Creator</h4>
+        <hr>
+        <h6>${instructorBio}</h6>
+      </div>
+    </div>
+
+    <h3>COMMENTS</h3>
+    <div class="comments-div">
+      <div class="comments"></div>
+      <div class="add-comment-btn-div">
+        <button class="add-comment">Add Comment</button>
+      </div>
+    </div>
+  </div>
+  `
 }
 
 function videosTemplate(video) {
